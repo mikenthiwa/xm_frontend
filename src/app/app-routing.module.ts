c383270657f10e56a37e5d6401578d049c60f53c
michael <mike.nthiwa@gmail.com>
2023-06-17 08:23:41 +0300

    Set up project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,30 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'registration',
+    loadChildren: () =>
+      import('./modules/registration/registration.module').then(
+        m => m.RegistrationModule
+      ),
+  },
+  {
+    path: 'welcome',
+    loadChildren: () =>
+      import('./modules/welcome/welcome.module').then(m => m.WelcomeModule),
+  },
+  {
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./modules/dashboard/dashboard.module').then(
+        m => m.DashboardModule
+      ),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
